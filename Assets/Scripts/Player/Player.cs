b75d268d27aead16fef408ee8bf64144cceb2e48
coolofdead <thomas.giovannoni@hotmail.fr>
2019-10-21 14:47:08 +0200

    personnage direction + fix bug mouvement

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -38,16 +38,7 @@ public class Player : MonoBehaviour
             animator.SetTrigger("sword hit");
         }
 
-        //var rotationAngle = transform.rotation.eulerAngles;
-        //transform.rotation = Quaternion.Euler(Input.mousePosition.x, rotationAngle.y, rotationAngle.z);
-        //Vector3 dir = transform.rotation.eulerAngles;
-        //dir.y = Input.mousePosition.normalized.y;
-        //transform.Rotate(dir, Space.World);
-
-        Vector3 lookDir = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Debug.Log(lookDir);
-        lookDir.y = transform.position.y;
-        var angle = Quaternion.Angle(Quaternion.Euler(lookDir), transform.rotation);
-        transform.rotation = Quaternion.AngleAxis(angle, transform.up);
+        var dir = (Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2, 0)).normalized;
+        transform.LookAt(transform.position + new Vector3(dir.x, 0, dir.y));
     }
 }
