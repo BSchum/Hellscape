57278ebe37c1e4ddd199c9ffb9c83d720ce64805
brice schumacher <brice.schumacher@ynov.com>
2019-10-25 00:55:10 +0200

    Réparation du mouvement et système de caractéristique

---
+++ Assets/Scripts/Motor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motor : MonoBehaviour
+{
+    public float speed;
+
+    public void Move(Vector3 direction)
+    {
+        transform.Translate(direction.normalized * Time.deltaTime * speed, Space.World);
+    }
+
+    public void Look(Vector3 pointToLook)
+    {
+        pointToLook.y = transform.position.y;
+        transform.LookAt(pointToLook);
+    }
+
+    public void LookAtMouse()
+    {
+        Camera camera = Camera.main;
+        Ray cameraRay = camera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+        Vector3 pointToLook = Vector3.zero;
+        if (groundPlane.Raycast(cameraRay, out float rayLength))
+        {
+            pointToLook = cameraRay.GetPoint(rayLength);
+        }
+        Look(pointToLook);
+    }
+
+}
---
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class BaseStats
+{
+    public float Power { get; set; }
+    public float Agility { get; set; }
+    public float Health { 
+        get {
+            return _health;
+        }
+        set { 
+            if(_health + value <= MaxHealth)
+            {
+                _health += value;
+            }
+            else
+            {
+                _health = MaxHealth;
+            }
+        } 
+    }
+    public float MaxHealth { get; set; }
+
+    private float _health { get; set; }
+
+    public static BaseStats operator +(BaseStats a, BaseStats b)
+    {
+        return new BaseStats()
+        {
+            Power = a.Power + b.Power,
+            Agility = a.Agility + b.Agility,
+            MaxHealth = a.MaxHealth + b.MaxHealth,
+            Health = a.Health + b.Health
+        };
+    }
+}
+
