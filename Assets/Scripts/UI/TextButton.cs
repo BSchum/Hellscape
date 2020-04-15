98835d54d3765a736217d22db0d00f8d22340d3f
Thomas <thomas.giovannoni@hotmail.fr>
2020-04-15 15:46:59 +0200

    btn menu

---
+++ Assets/Scripts/UI/TextButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextButton : MonoBehaviour
+{
+    public static Color c = new Color(255, 0, 0, 1);
+
+    private void Start()
+    {
+        GetComponent<Text>().color = c;
+    }
+}
