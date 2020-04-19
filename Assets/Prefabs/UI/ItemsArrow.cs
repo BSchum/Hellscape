ec50c0cf31cdd5410b3d9a1035b0326045b5838f
Thomas <thomas.giovannoni@hotmail.fr>
2020-04-19 04:32:02 +0200

    UI items + souls

---
+++ Assets/Prefabs/UI/ItemsArrow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsArrow : MonoBehaviour
+{
+    [SerializeField]
+    private bool state;
+    public Animator animator;
+
+    public void Display()
+    {
+        animator.SetBool("Display", state);
+
+        state = !state;
+    }
+}
--- Assets/Scripts/Manager/Managers.cs
+++ Assets/Scripts/Manager/Managers.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Managers : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(this.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
