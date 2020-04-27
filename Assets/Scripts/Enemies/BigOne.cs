025209c5ab932b043142281297872ec1face066c
brice schumacher <brice.schumacher@ynov.com>
2020-04-27 21:48:42 +0200

    Ne cumul plus les force du cac

--- Assets/Scripts/Enemies/BigOne.cs
+++ Assets/Scripts/Enemies/BigOne.cs
@@ -151,7 +151,12 @@ public class BigOne : Enemy
         var targets = Physics.OverlapSphere(this.transform.position, smashAttackRange).Where(c => c.tag == Constants.Tags.PLAYER_TAG);
         foreach (var target in targets)
         {
-            target.GetComponent<Rigidbody>().AddExplosionForce(1000f, this.transform.position, attackRange, 5f);
+            var playerRb = target.GetComponent<Rigidbody>();
+
+            if (playerRb.velocity.y < 900f)
+            {
+                target.GetComponent<Rigidbody>().AddExplosionForce(1000f, this.transform.position, attackRange, 5f);
+            }
         }
     }
     IEnumerator CircularAttack()
